aac51d68208203c0e776be4357353f06ba49fdef
Dealga McArdle <zeffii@users.noreply.github.com>
2025-11-15 14:15:41 +0000

    Improve selection and caret functionality
    Refactor selection handling and caret movement logic.

--- zen_tracker_ui.js
+++ zen_tracker_ui.js
@@ -13,7 +13,6 @@ var g_in_edit_mode = false;
 var g_key_codes = [];
 var g_mouse_on_rect = false;
 var g_caret = [0, 0]
-
 var start_x = 30;
 var start_y = 30;
 
@@ -23,22 +22,29 @@ var charheight = settings_font_size;
 
 var started_selection_mode = false;
 var g_updating_selection = true;
-var selection_range = [1, 1, 1, 1];  // x, y, w, h
 var selection_w_idx = 1;
 var selection_h_idx = 1;
 var sel_x_start = 30; 
 var sel_y_start = 30;
+var num_rows_selected = 1;
+var num_rows_selected = 1;
+
 
 function set_selection(x, y, w, h){
     selection_range = [x, y, w, h];
 }
 
+function print_abs_dims(){
+    post('num_rows_sel:', Math.abs(num_rows_selected), 'num_cols_sel:', Math.abs(num_cols_selected), '\n');
+}
+
 function draw_selection(){
-    if (!started_selection_mode){
-        return;
-    }
-    mgraphics.set_source_rgba(0.7, 0.7, 0.9, 1);  // almost black
-    mgraphics.rectangle(sel_x_start, sel_y_start, selection_w_idx * charwidth, selection_h_idx * charheight);
+    if (!started_selection_mode){ return; }
+    const [cx, cy] = caret_to_location();
+    mgraphics.set_source_rgba(0.7, 0.7, 0.9, 1);
+    var rect_width = (num_cols_selected-1) * charwidth;
+    var rect_height = num_rows_selected * charheight;
+    mgraphics.rectangle(cx-rect_width, cy-rect_height, rect_width+charwidth, rect_height);
     mgraphics.stroke();
 }
 
@@ -64,8 +70,8 @@ function caret_to_location(){
 function set_caret(x, y){ g_caret = [x, y]; }
 
 function reset_selection_range(){
-    selection_w_idx = 1;
-    selection_h_idx = 1;
+    num_rows_selected = 1;
+    num_cols_selected = 1;
 }
 
 function move_caret(direction){
@@ -85,59 +91,77 @@ function move_caret(direction){
         default:
             post('not possible');
     }
-    //  post(g_caret);
 }
 
 function update_selection_dimensions(direction){
-        switch (direction) {
-        case 28:  
-            if (g_caret[0] > 0){ selection_w_idx = selection_w_idx - 1 ; } // left
+    const num_cols_in_pattern = 25;
+    const num_rows_in_pattern = 16;
+    switch (direction) {
+        case 28: // left
+            if (g_caret[0] > 0){ 
+                num_cols_selected -= 1;
+            } 
             break;
-        case 29: 
-            if (g_caret[0] < 24){ selection_w_idx = selection_w_idx + 1; } // right
+        case 29: // right
+            if (g_caret[0] < (num_cols_in_pattern-1)){ 
+                num_cols_selected += 1;
+            } 
             break;
-        case 30: 
-            if (g_caret[1] > 0){ selection_h_idx = selection_h_idx - 1; } // up
+        case 30: // up
+            if (g_caret[1] > 0){ 
+                num_rows_selected -= 1;
+            } 
             break;
-        case 31: 
-            if (g_caret[1] < 15){ selection_h_idx = selection_h_idx + 1; } // down
+        case 31: // down
+            if (g_caret[1] < (num_rows_in_pattern)){ 
+                if (num_rows_selected !== num_rows_in_pattern){
+                    num_rows_selected += 1;
+                }
+            } 
             break;
         default:
             post('not possible');
     }
-    post('selection', selection_w_idx, selection_h_idx);
 }
 
 function key_handler(){
     if (g_in_edit_mode){
-        var directions = [28, 29, 30, 31];
-        if (directions.indexOf(g_key_codes[0]) !== -1){
-            move_caret(g_key_codes[0]);
-        }
 
         var just_shift = 512;
-        if (g_key_codes[2] === just_shift){
+        var directions = [28, 29, 30, 31];
+        var direction_input = (directions.indexOf(g_key_codes[0]) !== -1);
+
+        if (direction_input){
+            move_caret(g_key_codes[0]);
+        }
+        
+        if ((g_key_codes[2] === just_shift) && direction_input){
             if (started_selection_mode){
                 post('modifying');
                 update_selection_dimensions(g_key_codes[0]);
-                set_selection(sel_x_start, sel_y_start, selection_w_idx * charwidth, selection_h_idx * charheight);
+                //const [x1, y1, x2, y2] = g_selection_range_in_carets;
+                //var cx = start_x + ((4 + g_caret[0]) * charwidth);
+                //var cy = start_y + (g_caret[1] * charheight);
+                //set_selection(cx, cy, (x2 - x1 - 1) * charwidth, ((y2 - y1) * charheight) - charheight);
             } else {
                 // means the section is going to start at cursor index x, y, w, h where w and h are 1.
-                reset_selection_range();
-                var decomposexy = caret_to_location()
-                sel_x_start = decomposexy[0]; 
-                sel_y_start = decomposexy[1];
-                set_selection(sel_x_start, sel_y_start, charwidth, charheight);
+                update_selection_dimensions(g_key_codes[0]);
+                //const [x_start, y_start] = caret_to_location();
+                //set_selection(x_start, y_start, charwidth, charheight);
                 post('starting');
             }
             started_selection_mode = true;
+
         } else {
+            reset_selection_range();
             started_selection_mode = false;
         }
-
+        // post('selection range: ', g_selection_range_in_carets);
         // we can restrict this to redraw iff there are updates, but for now this is convenient.
         mgraphics.redraw();
     }
+    // post('woop', num_cols_selected, num_rows_selected, '\n');
+    print_abs_dims();
 
 }
 
