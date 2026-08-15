37ad940864de1df1c6d50f876ec76c62dbd00088
Dealga <tetha.z@gmail.com>
2026-08-15 20:41:27 +0100

    add help

--- ztrk_envelope_ui.js
+++ ztrk_envelope_ui.js
@@ -8,7 +8,7 @@ mgraphics.autofill = 0;
 const theme_colors = {};
 theme_colors.main_bg_color = [0.2, 0.2, 0.2, 1.0];
 theme_colors.stroke_color = [0.7, 0.3, 0.2, 1.0];
-theme_colors.stroke_color_dark = [0.3, 0.3, 0.3, 1.0];
+theme_colors.stroke_color_dark = [0.18, 0.18, 0.18, 1.0];
 theme_colors.active_node = [0.3, 0.6, 0.7, 1.0];
 theme_colors.node = [0.8, 0.8, 0.7, 1.0];
 theme_colors.info_text = [0.3, 0.6, 0.97, 1.0];
@@ -26,6 +26,8 @@ draw_data.coords = {};
 const config = {};
 config.font_size = 12;
 config.font_descriptor = ["Consolas", "normal", "normal"];
+config.display_help = false;
+
 mgraphics.set_font_size(config.font_size);
 mgraphics.select_font_face(...config.font_descriptor);
 // charwidth = gfx.text_measure('_')[0];
@@ -98,6 +100,24 @@ function draw_node_info(gfx, w, h){
     gfx.show_text(text_content);
 }
 
+function draw_node_help(gfx, w, h){
+    const help_lines = [
+        "Spacebar : Enter edit mode",
+        "Arrows : Go to next or previous node",
+        "Shift+Arrows : Move current node around",
+        "S : Enable sustain mode on current node",
+        "Del : Remove current node",
+        "H : Show/Hide this message"
+    ];
+    gfx.set_source_rgba(...theme_colors.info_text);
+    var [text_width, text_height] = gfx.text_measure("X");
+    for (const idx in help_lines){
+        gfx.move_to(2*padding, (2*padding) + (idx * text_height));
+        gfx.show_text(help_lines[idx]);
+    }
+
+};
+
 function paint() {
     // state and setup     
     box.size(500, 200);
@@ -109,11 +129,18 @@ function paint() {
     calculate_node_locations(gfx, w, h);
 
     fill_background(gfx, w, h);
-    gfx.set_line_width(2.0);
-    draw_bounding_rect(gfx, w, h);
-    draw_lines(gfx, w, h);
-    draw_nodes(gfx, w, h);
-    draw_node_info(gfx, w, h);
+    
+    if (!(config.display_help)){
+        gfx.set_line_width(2.0);
+        draw_bounding_rect(gfx, w, h);
+        draw_lines(gfx, w, h);
+        draw_nodes(gfx, w, h);
+        draw_node_info(gfx, w, h);
+    } else {
+        draw_node_help(gfx, w, h);
+    }
+
+
 }
 
 function onresize(w, h) {
@@ -121,11 +148,18 @@ function onresize(w, h) {
     refresh(); 
 }
 
+// custom mesages
+
 function move_node(arg1, arg2, arg3){
     if (!(arg1 in env_nodes)) return;       // node index check, dont ref nodes that dont exist
     if (arg2 > 1.0 || arg2 < 0.0) return;   // bound check for x
     if (arg3 > 1.0 || arg3 < 0.0) return;   // bound check for x
     
     env_nodes[arg1] = [arg2, arg3];
+    refresh(); 
+}
+
+function display_help(){
+    config.display_help = !config.display_help;
     refresh(); 
 }
