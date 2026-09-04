635f19a986afe96f96204eb34bcfecec2438ab7d
Dealga <tetha.z@gmail.com>
2026-09-04 22:05:17 +0100

    start defining the needed functions for buffer updates 2

--- ztrk_seq_utils.js
+++ ztrk_seq_utils.js
@@ -29,11 +29,6 @@ function write_track_buffer(props, array2d){
 
     var buff_track = new Buffer(props.buffer_name);
 
-    // these are set already.
-    // buff_track.setattr("sr", 1000);
-    // buff_track.setattr("chans", 24);
-    // buff_track.send("sizeinsamps", pattern_length);
-
     for (var row = 0; row < array2d.length; row++){
         for (var col = 0; col < array2d[0].length; col++){
             var celld = array2d[row][col];
