77ef465a464281682339b1d48be6b28c7dc4d307
Dealga <tetha.z@gmail.com>
2026-09-18 17:43:09 +0100

    more tweaks?

--- ztrk_console.js
+++ ztrk_console.js
@@ -25,34 +25,56 @@ var log_color = {
     'debug': [0.2, 0.9, 0.2, 1.0]
 };
 
-function get_text_width_and_height(){
-        var gfx = mgraphics;
-        gfx.set_font_size(ztrk_get_font_size());   //this.settings_font_size);
-        gfx.select_font_face(...ztrk_get_font_family());  //"Consolas", "normal", "normal");
-        [charwidth, charheight] = gfx.text_measure('A');
+var default_theme_colors = {
+    caret_color: [0.7, 0.2, 0.4, 1],
+    status_bg_color: [0.2, 0.3, 0.2, 1.0],
+    status_text_color: [0.9, 0.9, 0.7, 1.0]
+};
+
+function asRGB(r, g, b, a){ return {r:r, g:g, b:b}};
+function set_rgb(c, d){ mgraphics.set_source_rgba(c.r / d, c.g / d, c.b / d, 1); }
+
+function get_text_width_and_height(gfx){
+    gfx.set_font_size(ztrk_get_font_size());   //this.settings_font_size);
+    gfx.select_font_face(...ztrk_get_font_family());  //"Consolas", "normal", "normal");
+    [charwidth, charheight] = gfx.text_measure('/');
 }
 
-function dark_background(){
-    var gfx = mgraphics;
+function dark_background(gfx){
     var [w, h] = gfx.size;
-    // gfx.set_source_rgba(0.1, 0.2, 0.4, 1);  // this.ui_bg_color
     gfx.set_source_rgba(0.01, 0.07, 0.098, 1.0);
     gfx.rectangle(0, 0, w, h);
     gfx.fill();
 }
 
-function paint(){
-    var gfx = mgraphics;
-    get_text_width_and_height();
-    dark_background();
+function draw_status_bar(gfx){
+    var [w, h] = gfx.size;
+    // background.
+    set_rgb(asRGB(...default_theme_colors.status_bg_color), 2.3);
+    gfx.rectangle(0, h-charheight, w, charheight);
+    gfx.fill();
+    
+    // text
+    set_rgb(asRGB(...default_theme_colors.status_text_color), 1.3);
+    var ztrk_console_text = "zconsole 0.001";
+    var identifier_width = gfx.text_measure(ztrk_console_text + ' ')[0];
+    gfx.move_to(w - identifier_width, h - (0.25 * charheight));
+    gfx.show_text(ztrk_console_text);    
+}
 
+function draw_lines(gfx){
     for (const [idx, line] of output_list.entries()){
         gfx.set_source_rgba(...log_color[line[0]]);
         gfx.move_to(10, 20 + (idx * charheight));
         gfx.show_text(`${line[2]}: ${line[1]}`);
     }
-    mgraphics.redraw();
-    // ....
+}
+
+function paint(){
+    get_text_width_and_height(mgraphics);
+    dark_background(mgraphics);
+    draw_lines(mgraphics);
+    draw_status_bar(mgraphics);
 }
 
 function set_msg(...args){
@@ -60,10 +82,11 @@ function set_msg(...args){
     post("triggered!", received_string);
     output_list.push([args[0], received_string, line_idx]);
     line_idx += 1;
-
+    
     if (output_list.length >= 15){
         output_list.shift();
     }
+    mgraphics.redraw();
 }
 
 /*
